9902a9a10f79a0f2da543a64757f43d027d2706c
astrapi69 <asterios.raptis@gmx.net>
2015-11-23 16:03:42 +0100

    Moved jaulp.test.objects to its own repository.

---
+++ src/main/java/de/alpharogroup/BaseTestCase.java
@@ -0,0 +1,64 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2007 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.alpharogroup;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * The abstract class BaseTestCase is for tests in all packages 'de.alpharogroup.*'.
+ * 
+ * @version 1.0
+ * @author Asterios Raptis
+ */
+public class BaseTestCase
+{
+
+	/** The boolean result from the tests. */
+	protected boolean result;
+
+	/**
+	 * Sets up method will be invoked before every unit test method in this class.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@BeforeMethod
+	protected void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * Tear down method will be invoked after every unit test method in this class.
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	@AfterMethod
+	protected void tearDown() throws Exception
+	{
+	}
+
+}
---
+++ src/main/java/de/alpharogroup/test/objects/A.java
@@ -0,0 +1,83 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2007 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.alpharogroup.test.objects;
+
+/**
+ * The Class A.
+ */
+public class A
+{
+
+	/** The a. */
+	private String a;
+
+	/**
+	 * Returns <code>true</code> if this <code>A</code> is the same as the o argument.
+	 * 
+	 * @param o
+	 *            the o
+	 * @return is the same as the o argument.
+	 */
+	@Override
+	public boolean equals(final Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (o == null)
+		{
+			return false;
+		}
+		if (o.getClass() != getClass())
+		{
+			return false;
+		}
+		final A castedObj = (A)o;
+		return this.a == null ? castedObj.a == null : this.a.equals(castedObj.a);
+	}
+
+	/**
+	 * Gets the a.
+	 * 
+	 * @return the a
+	 */
+	public String getA()
+	{
+		return a;
+	}
+
+	/**
+	 * Sets the a.
+	 * 
+	 * @param a
+	 *            the new a
+	 */
+	public void setA(final String a)
+	{
+		this.a = a;
+	}
+
+}
