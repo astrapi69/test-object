8a77495d4e24f1c679f60c5f4ce4213a82cb1b1b
astrapi69 <asterios.raptis@gmx.net>
2021-12-21 10:29:44 +0100

    created new unit test class for showcase of nested nullable check

---
+++ src/test/java/io/github/astrapi69/test/OptionalTest.java
@@ -0,0 +1,41 @@
+package io.github.astrapi69.test;
+
+import io.github.astrapi69.test.objects.CompanyGroup;
+import io.github.astrapi69.test.objects.Employee;
+import io.github.astrapi69.test.objects.Factory;
+import io.github.astrapi69.test.objects.enums.Gender;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class OptionalTest {
+
+    @Test
+    public void testNestedOptionalOfNullable() {
+        CompanyGroup companyGroup = CompanyGroup.builder().build();
+        Optional<String> optionalGenderName = resolve(() -> companyGroup.getParent().getCeo().getPerson().getGender().name());
+
+        assertFalse(optionalGenderName.isPresent());
+    }
+
+    public static <T> Optional<T> resolve(Supplier<T> resolver) {
+        try {
+            T result = resolver.get();
+            return Optional.ofNullable(result);
+        }
+        catch (NullPointerException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static String getCeoGender(CompanyGroup companyGroup) {
+        return Optional.ofNullable(companyGroup).map(cg -> cg.getParent())
+                .map(parentFactory -> parentFactory.getCeo())
+                .map(ceo -> ceo.getPerson())
+                .map(person -> person.getGender())
+                .map(gender -> gender.name())
+                .orElse(Gender.UNDEFINED.name());
+    }
+}
